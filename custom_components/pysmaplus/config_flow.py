77ef225992a56830cfaa637c7c0af133581d5b0b
Sven Bursch-Osewold <sb_git@bursch.com>
2025-09-23 18:23:59 +0200

    flow config_entry, fixes #40

--- custom_components/pysmaplus/config_flow.py
+++ custom_components/pysmaplus/config_flow.py
@@ -66,9 +66,8 @@ async def validate_input(
 class PySMAOptionsConfigFlow(config_entries.OptionsFlow):
     """Handle a pyscript options flow."""
 
-    def __init__(self, config_entry: ConfigEntry) -> None:
+    def __init__(self) -> None:
         """Initialize pyscript options flow."""
-        self.config_entry = config_entry
         self._show_form = False
 
     async def async_step_init(
@@ -147,9 +146,9 @@ class SmaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):  # type: ignore[c
 
     @staticmethod
     @callback
-    def async_get_options_flow(config_entry: ConfigEntry) -> PySMAOptionsConfigFlow:
+    def async_get_options_flow() -> PySMAOptionsConfigFlow:
         """Get the options flow for this handler."""
-        return PySMAOptionsConfigFlow(config_entry)
+        return PySMAOptionsConfigFlow()
 
     # "User" => 0,1,2,4 => details => selection
     #        => 3 => selection
